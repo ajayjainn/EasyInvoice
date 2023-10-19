a2ba31304abb98e3d1c758cf0d610616ae572256
Ajay <ajayjain1024@gmail.com>
2023-10-19 23:56:19 +0530

    feat: setup nginx reverse proxy with docker

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import cookieParser from 'cookie-parser'
 import 'dotenv/config'
 import express from 'express'
 import morgan from 'morgan'
-import { morganMiddleware,systemLogs } from './utils/logger.js'
+import { morganMiddleware, systemLogs } from './utils/logger.js'
 
 const app = express()
 
@@ -21,7 +21,7 @@ app.get('/api/v1/test', (req, res) => {
   res.json({ Hi: 'Welcome to Invoice App' })
 })
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 1997
 const NODE_ENV = process.env.NODE_ENV || 'development'
 
 app.listen(PORT, () => {
